790d4fe5a1aee176694f3aadb1a5bb8526a50eab
Lucas Garron <code@garron.net>
2024-11-07 16:05:14 -0800

    Partial test fix.

--- test/util.ts
+++ test/util.ts
@@ -1,21 +1,16 @@
-import path from "node:path";
 import { type ExecaReturnValue, execa } from "execa";
 
 export function app(
   scriptPath: string,
 ): (args: string[]) => Promise<ExecaReturnValue> {
   return async (args) => {
-    const result = await execa(
-      path.join(__dirname, "../scripts/ts-node"),
-      [scriptPath, ...args],
-      {
-        all: true,
-        reject: false,
-        env: {
-          FORCE_COLOR: "true",
-        },
+    const result = await execa("bun", ["run", scriptPath, ...args], {
+      all: true,
+      reject: false,
+      env: {
+        FORCE_COLOR: "true",
       },
-    );
+    });
     return result;
   };
 }
