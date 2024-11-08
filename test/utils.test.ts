9e6a84ba97988ca38c9cff99fb60271fea23ffc6
Lucas Garron <code@garron.net>
2024-11-07 17:42:08 -0800

    Remove a whole bunch of dependencies.

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "bun:test";
-import chalk from "chalk";
+import chalkTemplate from "chalk-template";
 import stripAnsi from "strip-ansi";
 import { type AllOrNothing, padNoAnsi } from "../src/utils";
 
@@ -7,7 +7,7 @@ describe("padNoAnsi", () => {
   it("pads start", () => {
     const expected = "hello".padStart(10, " ");
     const actual = padNoAnsi(
-      chalk`{red h}{cyan e}{blue l}{green l}{red o}`,
+      chalkTemplate`{red h}{cyan e}{blue l}{green l}{red o}`,
       10,
       "start",
     );
@@ -16,14 +16,14 @@ describe("padNoAnsi", () => {
   it("pads end", () => {
     const expected = "hello".padEnd(10, " ");
     const actual = padNoAnsi(
-      chalk`{red h}{cyan e}{blue l}{green l}{red o}`,
+      chalkTemplate`{red h}{cyan e}{blue l}{green l}{red o}`,
       10,
       "end",
     );
     expect(stripAnsi(actual)).toEqual(expected);
   });
   it("returns the string if it is shorter than the padding", () => {
-    const str = chalk`{red h}{cyan e}{blue l}{green l}{red o}`;
+    const str = chalkTemplate`{red h}{cyan e}{blue l}{green l}{red o}`;
     const actual = padNoAnsi(str, 2, "end");
     expect(actual).toEqual(str);
   });
